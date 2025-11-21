d81bade857a33c80d56c49d3dc30b7b35cc469e8
EricaCamelopardalis <clarajs2017@outlook.com>
2025-11-21 10:54:43 -0600

    Added Jupyter notebooks and mapping files

---
+++ SI_Course_Map.py
@@ -0,0 +1,118 @@
+course_map = {
+    "CHEM 1103": {
+        "New_Code": "CHEM 14103",
+        "Title": "University Chemistry I"
+    },
+    "CHEM 1123": {
+        "New_Code": "CHEM 14203",
+        "Title": "University Chemistry II"
+    },
+    "CHEM 1123H": {
+        "New_Code": "CHEM 142H3",
+        "Title": "Honors Chemistry II"
+    },
+    "CHEM 1073": {
+        "New_Code": "CHEM 12103",
+        "Title": "Fundamentals of Chemistry"
+    },
+    "CHEM 1073 Online": {
+        "New_Code": "CHEM 12103 Online",
+        "Title": "Fundamentals of Chemistry Online"
+    },
+    "CHEM 3603": {
+        "New_Code": "CHEM 36053",
+        "Title": "Organic Chemistry I"
+    },
+    "BIOL 1543": {
+        "New_Code": "BIOL 10103",
+        "Title": "Principles of Biology"
+    },
+    "CHEM 2613": {
+        "New_Code": "CHEM 26103",
+        "Title": "Organic Physiological Chemistry"
+    },
+    "CHEM 3613": {
+        "New_Code": "CHEM 36203",
+        "Title": "Organic Chemistry II"
+    },
+    "NURS 3314": {
+        "New_Code": "NURS 33174",
+        "Title": "Pathophysiology"
+    },
+    "UNIV 1001": {
+        "New_Code": "UNIV 10051",
+        "Title": "University Perspectives"
+    },
+    "ACCT 2013": {
+        "New_Code": "ACCT 20103",
+        "Title": "Accounting Principles"
+    },
+    "ACCT 2013 Online": {
+        "New_Code": "ACCT 20103 Online",
+        "Title": "Accounting Principles Online"
+    },
+    "ACCT 2023": {
+        "New_Code": "ACCT 20203",
+        "Title": "Accounting Principles II"
+    },
+    "ACCT 2023 Online": {
+        "New_Code": "ACCT 20203 Online",
+        "Title": "Accounting Principles II Online"
+    },
+    "ECON 2013": {
+        "New_Code": "ECON 21003",
+        "Title": "Principles of Macroeconomics"
+    },
+    "ECON 2013 Online": {
+        "New_Code": "ECON 21003 Online",
+        "Title": "Principles of Macroeconomics Online"
+    },
+    "ECON 2013H": {
+        "New_Code": "ECON 2100H3",
+        "Title": "Honors Principles of Macroeconomics"
+    },
+    "ECON 2023": {
+        "New_Code": "ECON 22003",
+        "Title": "Principles of Microeconomics"
+    },
+    "ECON 2023 Online": {
+        "New_Code": "ECON 22003 Online",
+        "Title": "Principles of Microeconomics Online"
+    },
+    "ECON 2023H": {
+        "New_Code": "ECON 220H3",
+        "Title": "Honors Principles of Microeconomics"
+    },
+    "ECON 2143": {
+        "New_Code": "ECON 21403",
+        "Title": "Basic Economics: Theory and Practice"
+    },
+    "ECON 2143 Online": {
+        "New_Code": "ECON 21403 Online",
+        "Title": "Basic Economics: Theory and Practice Online"
+    },
+    "ECON 2143H": {
+        "New_Code": "ECON 214H3",
+        "Title": "Honors Basic Economics: Theory and Practice"
+    },
+    "EXSC 3353": {
+        "New_Code": "EXSC 33503",
+        "Title": "Mechanics of Human Movement"
+    },
+    "CHEG 2113": {
+        "New_Code": "CHEG 21103",
+        "Title": "Introduction to Chemical Engineering I"
+    },
+    "CHEG 2133": {
+        "New_Code": "CHEG 21303",
+        "Title": "Fluid Mechanics"
+    },
+    "MATH 2043": {
+        "New_Code": "MATH 22003",
+        "Title": "Survey of Calculus"
+    },
+    "WCOB 1033": {
+        "New_Code": None,
+        "Title": "Data Analysis"
+    }
+}
